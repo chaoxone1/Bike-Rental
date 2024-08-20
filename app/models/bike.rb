4540ab44b9122e51125dbf3379c65b0397efcee4
Adrien Kropfeld <adrien.kropfeld@googlemail.com>
2024-08-20 12:17:12 +0200

    made controllers working and added Seedfile wih faker

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,6 +1,6 @@
 class Bike < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo
+  # has_one_attached :photo
   validates :description, length: { minimum: 10 }
   validates :description, :price, presence: true
 end
